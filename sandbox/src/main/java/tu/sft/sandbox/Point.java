b10dd8379ee8bbf5baa27adc78f079ecc7c10073
glavatskiy <a.glavatskiy@cft.ru>
2020-11-27 22:02:52 +0700

    Результат выполнения задания 2

---
+++ sandbox/src/main/java/tu/sft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package tu.sft.sandbox;
+
+public class Point {
+
+	int x;
+	int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double distance(Point p) {
+		return Math.sqrt(Math.pow((p.x - this.x),2) + Math.pow((p.y - this.y),2));
+	}
+}
---
+++ sandbox/src/main/java/tu/sft/sandbox/Task2.java
@@ -0,0 +1,18 @@
+package tu.sft.sandbox;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		Point p1 = new Point(10,10);
+		Point p2 = new Point(151,151);
+
+		System.out.println("Distance is " + distance(p1,p2));
+		System.out.println("Distance from method on class Point is " +p1.distance(p2));
+	}
+
+
+	private static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+	}
+}
