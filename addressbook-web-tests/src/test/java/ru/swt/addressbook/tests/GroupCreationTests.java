1de37c719e000b263be869fa10f9b2dfd865d28d
glavatskiy <a.glavatskiy@cft.ru>
2021-01-20 14:14:47 +0700

    Добавил проверку сравнения списков групп+состав групп при удалении

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTests.java
@@ -4,17 +4,19 @@ import org.junit.Assert;
 import ru.swt.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
 	@Test
 	public void testGroupCreation() throws InterruptedException {
 		app.getNavigationHelper().goToGroupPage();
 
-		int before = app.getGroupHelper().getGroupCount();
+		List<GroupData> before = app.getGroupHelper().getGroupList();
 
 		app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
 
-		int after = app.getGroupHelper().getGroupCount();
-		Assert.assertEquals(after, before + 1);
+		List<GroupData> after = app.getGroupHelper().getGroupList();
+		Assert.assertEquals(after.size(), before.size() + 1);
 	}
 }
--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupDeletionTests.java
@@ -4,22 +4,27 @@ import org.junit.Assert;
 import org.testng.annotations.Test;
 import ru.swt.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
 	@Test
 	public void testGroupDelete() throws Exception {
 		app.getNavigationHelper().goToGroupPage();
-		if(!app.getGroupHelper().isThereAGroup()){
+		if (!app.getGroupHelper().isThereAGroup()) {
 			app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
 		}
 
-		int before = app.getGroupHelper().getGroupCount();
+		List<GroupData> before = app.getGroupHelper().getGroupList();
 
-		app.getGroupHelper().selectGroup(before - 1);
+		app.getGroupHelper().selectGroup(before.size() - 1);
 		app.getGroupHelper().deleteSelectedGroups();
 		app.getNavigationHelper().goToGroupPage();
 
-		int after = app.getGroupHelper().getGroupCount();
-		Assert.assertEquals(after, before - 1);
+		List<GroupData> after = app.getGroupHelper().getGroupList();
+		Assert.assertEquals(after.size(), before.size() - 1);
+
+		before.remove(before.size() - 1);
+		Assert.assertEquals(before, after);
 	}
 }
