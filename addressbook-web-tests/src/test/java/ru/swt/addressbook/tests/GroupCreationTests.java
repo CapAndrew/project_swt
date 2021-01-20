c367e986da0f5b161bc99f3ad6ed1fed3e3de844
glavatskiy <a.glavatskiy@cft.ru>
2021-01-20 15:31:07 +0700

    Изменил способ нахождения maxId группы

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTests.java
@@ -1,8 +1,8 @@
 package ru.swt.addressbook.tests;
 
 import org.junit.Assert;
-import ru.swt.addressbook.model.GroupData;
 import org.testng.annotations.Test;
+import ru.swt.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -15,19 +15,13 @@ public class GroupCreationTests extends TestBase {
 
 		List<GroupData> before = app.getGroupHelper().getGroupList();
 
-		GroupData group = new GroupData("test1", "test2", "test3");
+		GroupData group = new GroupData("test3", "test2", "test3");
 		app.getGroupHelper().createGroup(group);
 
 		List<GroupData> after = app.getGroupHelper().getGroupList();
 		Assert.assertEquals(after.size(), before.size() + 1);
 
-		int max = 0;
-		for (GroupData g : after) {
-			if (g.getId() > max) {
-				max = g.getId();
-			}
-		}
-		group.setId(max);
+		group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 		before.add(group);
 		Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 	}
