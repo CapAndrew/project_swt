03b9b0ff6fea4c45e09dac6179c8001c9b0901a6
glavatskiy <a.glavatskiy@cft.ru>
2021-01-20 14:25:27 +0700

    Добавил проверку состава групп в тесте на изменение

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.testng.annotations.Test;
 import ru.swt.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -19,11 +20,16 @@ public class GroupModificationTests extends TestBase {
 
 		app.getGroupHelper().selectGroup(before.size() - 1);
 		app.getGroupHelper().initGroupModification();
-		app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
+		GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test4", "test5", "test6");
+		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupModification();
 		app.getGroupHelper().returnToGroupPage();
 
 		List<GroupData> after = app.getGroupHelper().getGroupList();
 		Assert.assertEquals(after.size(), before.size());
+
+		before.remove(before.size() - 1);
+		before.add(group);
+		Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 	}
 }
