1be3f9b544941d19dfdbe830cdf2be21d3b03c7e
glavatskiy <a.glavatskiy@cft.ru>
2021-01-20 12:58:32 +0700

    Добавил проверку на кол-во групп до и после действий теста

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.swt.addressbook.tests;
 
+import org.junit.Assert;
 import ru.swt.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
@@ -8,6 +9,12 @@ public class GroupCreationTests extends TestBase {
 	@Test
 	public void testGroupCreation() throws InterruptedException {
 		app.getNavigationHelper().goToGroupPage();
+
+		int before = app.getGroupHelper().getGroupCount();
+
 		app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+
+		int after = app.getGroupHelper().getGroupCount();
+		Assert.assertEquals(after, before + 1);
 	}
 }
--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.swt.addressbook.tests;
 
+import org.junit.Assert;
 import org.testng.annotations.Test;
 import ru.swt.addressbook.model.GroupData;
 
@@ -11,8 +12,14 @@ public class GroupDeletionTests extends TestBase {
 		if(!app.getGroupHelper().isThereAGroup()){
 			app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
 		}
-		app.getGroupHelper().selectGroup();
+
+		int before = app.getGroupHelper().getGroupCount();
+
+		app.getGroupHelper().selectGroup(before - 1);
 		app.getGroupHelper().deleteSelectedGroups();
 		app.getNavigationHelper().goToGroupPage();
+
+		int after = app.getGroupHelper().getGroupCount();
+		Assert.assertEquals(after, before - 1);
 	}
 }
