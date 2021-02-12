167e3f812d1f089de7a313d7b6de1590bd79ce5e
glavatskiy <a.glavatskiy@cft.ru>
2021-02-12 11:54:15 +0700

    Изменил значение в тесте, чтобы получить ошибку при выполнении 2 попытка

--- sandbox/src/test/java/ru/swt/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/swt/sandbox/PointTest.java
@@ -15,7 +15,7 @@ public class PointTest {
 	@Test
 	public void testDistanceBetweenPointsWithPositivesCoordinates(){
 		Point p1 = new Point(0, 0);
-		Point p2 = new Point(100, 0);
+		Point p2 = new Point(100, 3);
 		Assert.assertEquals(100.0, p1.distance(p2), 0);
 	}
 
